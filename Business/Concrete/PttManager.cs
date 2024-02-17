bf7f060c12ae926b0f73410089d4cdc2266dc652
Bilgehan Yaradanakul <bilgehan.yaradanakul60@gmail.com>
2024-02-17 02:56:08 +0200

    Add project files.

---
+++ Business/Concrete/PttManager.cs
@@ -0,0 +1,29 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class PttManager : ISupplierService
+    {
+        public IApplicantService _applicantService;
+
+           public PttManager(IApplicantService applicantService)
+        {
+            _applicantService = applicantService;
+        }
+    
+        public void GiveMask(Person person)
+        {
+
+            if (_applicantService.CheckPerson(person))
+            {
+                Console.WriteLine("Maskeniz verildi Sayin "+ person.LastName);
+            }
+        }
+    }
+}
---
+++ Workaround/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Business.Concrete;
+using Entities.Concrete;
+
+Console.WriteLine("Hello, World!");
+
+Person person1 = new Person();
+person1.FirstName = "Bilgehan";
+person1.LastName = "Yaradanakul";
+person1.NationalIdentity = 1123123123;
+person1.DateOfBirthYear = 2004;
+
+PttManager pttManager = new PttManager (new PersonManager());
+pttManager.GiveMask(person1);
+
+
+
+ 
+
+
+
+
